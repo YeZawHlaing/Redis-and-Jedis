369857e9887769c63e11e2acba371b13dbbff167
YeZawHlaing <yezawhlaing0@gmail.com>
2024-11-08 02:52:52 +0630

    initial commit

---
+++ Redis-server/src/test/java/com/backend/Redis/server/RedisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Redis.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
